9463d5f0c2b0c34d3aa8f3491618fc589a580786
SantiagoPunales <santiago.punales@correo.ucu.edu.uy>
2024-10-21 09:51:49 -0300

    TestEnemigos

---
+++ test/LibraryTests/TestGanondorf.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace roleplay;
+
+public class TestGanondorf
+{
+    private Ganondorf ganondorf;
+
+    [SetUp]
+    public void Setup()
+    {
+        ganondorf = new Ganondorf("Ganondorf", false);
+    }
+
+    [Test]
+    public void TestInicializacionGanondorf()
+    {
+        Assert.That(ganondorf.Nombre, Is.EqualTo("Ganondorf"));
+        Assert.That(ganondorf.Vida, Is.EqualTo(200));
+        Assert.That(ganondorf.Vidabase, Is.EqualTo(200));
+        Assert.That(ganondorf.Ataque, Is.EqualTo(20));
+    }
+
+    [Test]
+    public void TestInicializacionGanondorfFormaFinal()
+    {
+        var ganondorfFinal = new Ganondorf("Ganondorf", true);
+        Assert.That(ganondorfFinal.Nombre, Is.EqualTo("Ganondorf -Forma Final-"));
+        Assert.That(ganondorfFinal.Vida, Is.EqualTo(230)); // Vida base + 30
+        Assert.That(ganondorfFinal.Vidabase, Is.EqualTo(230)); // Vida base + 30
+        Assert.That(ganondorfFinal.Ataque, Is.EqualTo(70)); // Ataque base + 50
+    }
+}
---
+++ test/LibraryTests/TestLizalfos.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+namespace roleplay;
+
+public class TestLizalfos
+{
+    private Lizalfos lizalfos;
+
+    [SetUp]
+    public void Setup()
+    {
+        lizalfos = new Lizalfos("Zelda");
+    }
+
+    [Test]
+    public void TestInicializacionLizalfos()
+    {
+        Assert.That(lizalfos.Nombre, Is.EqualTo("Zelda"));
+        Assert.That(lizalfos.Vida, Is.EqualTo(200));
+        Assert.That(lizalfos.Vidabase, Is.EqualTo(200));
+        Assert.That(lizalfos.Ataque, Is.EqualTo(20));
+    }
+
+    [Test]
+    public void TestRecibirAtaqueFisico()
+    {
+        lizalfos.RecibirAtaqueFisico(30, "Link");
+        Assert.That(lizalfos.Vida, Is.EqualTo(170)); // Verifica que la vida disminuya correctamente
+    }
+
+    [Test]
+    public void TestRecibirAtaqueFisicoYVerificarMuerte()
+    {
+        lizalfos.RecibirAtaqueFisico(200, "Link"); // Ataque que debería matar al Lizalfos
+        Assert.That(lizalfos.Vida, Is.EqualTo(-30)); // Verifica que la vida sea -30
+    }
+
+    [Test]
+    public void TestRecibirAtaqueMagico()
+    {
+        var output = CaptureConsoleOutput(() => lizalfos.RecibirAtaqueMagico(50, "Zelda"));
+        Assert.That(output, Is.EqualTo("Zelda es inmune a los ataques magicos. El ataque de Zelda no tuvo efecto.\n"));
+        Assert.That(lizalfos.Vida, Is.EqualTo(200)); // La vida no debe cambiar
+    }
+
+    private string CaptureConsoleOutput(Action action)
+    {
+        var sw = new StringWriter();
+        Console.SetOut(sw);
+        action();
+        return sw.ToString();
+    }
+}
